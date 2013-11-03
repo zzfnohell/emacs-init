70fe62f18595194a16af844236569c2d865d3a41
JohnZhang <johnzhang@zzfnohell-VirtualBox>
2013-11-03 16:19:43 +0800

    change quicklisp path

--- slime.el
+++ slime.el
@@ -1,6 +1,6 @@
 ;; enable quicklisp in slime mode.
 (defvar *slime-helper-path*
-  (concat *workspace* "/libs/quicklisp/slime-helper.el"))
+  (concat *workspace* "quicklisp/slime-helper.el"))
 ;;(load (expand-file-name *slime-helper-path*))
 
 ;; set lisp implements on different platforms.
