5f9d9c0b4c429f9f994ec7c6d1d1bad907321c0f
JohnZhang <johnzhang@zzfnohell-VirtualBox>
2013-11-11 04:30:07 +0800

    cygwin path

--- init-parameters.el
+++ init-parameters.el
@@ -4,8 +4,8 @@
 (defvar *semantic-include-directories* nil)
 (defvar *semantic-preprocessor-directories* nil)
 (defvar *cedet-user-include-dirs* nil)
-(defvar *cygwin-root* "c:/cygwin")
-(defvar *cygwin-bin* "c:/cygwin/bin")
+(defvar *cygwin-root* "d:/cygwin")
+(defvar *cygwin-bin* "d:/cygwin/bin")
 (defvar *rfc-directory* (concat *workspace* "/rfc"))
 
 (defun add-subdirs-to-load-path
@@ -13,8 +13,6 @@
   (let ((default-directory (concat dir "/")))
     (setq load-path (cons dir load-path))
     (normal-top-level-add-subdirs-to-load-path)))
-
-(if (cygwin-p) (setq exec-path (cons *cygwin-bin* exec-path)))
 
 (setq default-fill-column 80)
 
