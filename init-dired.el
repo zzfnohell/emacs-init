5ad123c91eae6ce0f120bfc1cb9a17da8fd67580
JohnZhang <johnzhang@zzfnohell-VirtualBox>
2013-11-11 04:45:13 +0800

    dired

--- init-dired.el
+++ init-dired.el
@@ -1,3 +1,3 @@
 ;; set dired mode format,put it before the desktop load
-(setq dired-listing-switches "-avl --time-style=long-iso")))
+(setq dired-listing-switches "-avl --time-style=long-iso")
 (provide 'init-dired)
