46bae2b3b82c311f4766073a682262df5aacc4e5
JohnZhang <johnzhang@zzfnohell-VirtualBox>
2013-11-11 04:39:51 +0800

    powershell

--- init-powershell.el
+++ init-powershell.el
@@ -1,10 +1,9 @@
-(cond 
- ((windows-p) 
-  (require 'powershell) 
-  (require 'powershell-mode)))
+(require-package 'powershell)
+(require-package 'powershell-mode)
+
+(require 'powershell) 
+(require 'powershell-mode)
 
 (setq auto-mode-alist (cons '("\\.ps1" . powershell-mode) auto-mode-alist))
-
-
 
 (provide 'init-powershell)
