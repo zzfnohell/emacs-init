f0cafea3199c9446e2c161c73237e8e023bda74a
zzfnohell <zzfnohell@zzfnohell-VirtualBox>
2013-11-30 16:38:14 +0800

    init yasnippet

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -1,4 +1,10 @@
 (require 'yasnippet)
-(yas-global-mode 1)
 
+(setq
+ yas-snippet-dirs
+ '("~/.emacs.d/yasnippet/snippets"
+   "~/.emacs.d/yasnippet/yasnippet-snippets"
+   "~/.emacs.d/yasnippet/yasmate/snippets"))
+
+(yas-global-mode 1)
 (provide 'init-yasnippet)
