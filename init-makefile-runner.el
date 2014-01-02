12cf3c67b7df7ba99c4454eb087bd670d3d9c2d9
zzfnohell <zzfnohell@zzfnohell-VirtualBox>
2014-01-03 01:31:03 +0800

    initialize makefile runner

---
+++ init-makefile-runner.el
@@ -0,0 +1,9 @@
+;;; init-makefile-runner.el --- Makefile Runner
+
+;;; Commentary:
+;; Makefile Runner
+(require-package 'makefile-runner)
+
+(provide 'init-makefile-runner)
+
+;;; init-makefile-runner.el ends here
