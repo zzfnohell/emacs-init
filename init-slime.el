d2658e9e3521604bcc078f85b7807c8aa1f3285d
Zongfeng Zhang <zzfnohell@gmail.com>
2014-01-18 22:27:42 +0800

    disable slime on platforms except gnu/linux

--- init-slime.el
+++ init-slime.el
@@ -1,6 +1,9 @@
-(setq inferior-lisp-program "sbcl")
-(defvar slime-helper-path "~/quicklisp/slime-helper.el")
-(load (expand-file-name slime-helper-path))
+(cond
+ ((eq system-type 'gnu/linux)
+  (defvar slime-helper-path "~/quicklisp/slime-helper.el")
+  (load (expand-file-name slime-helper-path))
+  (setq inferior-lisp-program "sbcl"))
+ (t t))
 
 (provide 'init-slime)
 
