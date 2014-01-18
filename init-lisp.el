8006f83d0210ee44603d2a8bd7d45653b9e75005
zzfnohell <zzfnohell@zzfnohell-VirtualBox>
2014-01-18 22:12:58 +0800

    use slime from quicklisp

--- init-lisp.el
+++ init-lisp.el
@@ -3,9 +3,6 @@
 (require-package 'lispy)
 (require-package 'diminish)
 (require 'diminish)
-(require-package 'elisp-slime-nav)
-(dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
-  (add-hook hook 'elisp-slime-nav-mode))
 
 (require-package 'lively)
 
--- init-slime.el
+++ init-slime.el
@@ -1,14 +1,6 @@
-(cond 
- ((eq system-type 'gnu/linux)
-  ;; enable quicklisp in slime mode.
-  (defvar *slime-helper-path*
-    (concat *workspace* "/quicklisp/slime-helper.el"))
-  
-  (load (expand-file-name *slime-helper-path*))
-  
-  ;; set lisp implements on different platforms.
-  (setq inferior-lisp-program "sbcl"))
- (t (message "quicklisp is not available.")))
+(setq inferior-lisp-program "sbcl")
+(defvar slime-helper-path "~/quicklisp/slime-helper.el")
+(load (expand-file-name slime-helper-path))
 
 (provide 'init-slime)
 
