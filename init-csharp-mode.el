3943c11fb9070dd5bd1e13bea768d9bae0c4d3ad
Zongfeng Zhang <zzfnohell@gmail.com>
2014-01-21 15:01:13 +0800

    csharp mode

--- init-csharp-mode.el
+++ init-csharp-mode.el
@@ -1,13 +1,17 @@
+(require 'cc-mode)
+(require-package 'csharp-mode)
 (autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
 
 (setq auto-mode-alist
       (append '(("\\.cs$" . csharp-mode)) auto-mode-alist))
 
+(require-package 'yasnippet)
 (defun my-csharp-mode-fn ()
-  "function that runs when csharp-mode is initialized for a buffer.")
-
+     "function that runs when csharp-mode is initialized for a buffer."
+     (turn-on-auto-revert-mode)
+     (setq indent-tabs-mode nil)
+     (yas-minor-mode-on))
+  
 (add-hook  'csharp-mode-hook 'my-csharp-mode-fn t)
-
-
 
 (provide 'init-csharp-mode)
