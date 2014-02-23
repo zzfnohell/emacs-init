e3d7c52b4e2bd1b264b106cfea30d165569e5d66
zzfnohell <zzfnohell@zzfnohell-VirtualBox>
2014-02-23 14:00:08 +0800

    disable axiom

--- init-axiom.el
+++ init-axiom.el
@@ -4,9 +4,10 @@
 ;; Axiom
 
 ;;; Code:
-(load-file "~/.emacs.d/site-lisp/axiom-environment/axiom.el")
-(custom-set-variables
- '(axiom-process-program "axiom -noht"))
+;;(load-file "~/.emacs.d/site-lisp/axiom-environment/axiom.el")
+
+;; (custom-set-variables
+;;  '(axiom-process-program "axiom -noht"))
 
 (provide 'init-axiom)
 ;;; init-axiom.el ends here
