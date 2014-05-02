9b13087b52a6ecc36358f272ca58fd0129d0b49f
John Z <zzfnohell@gmail.com>
2014-05-02 19:48:42 +0800

    angular yasnippet

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -26,23 +26,20 @@
 (defun yas-popup-isearch-prompt (prompt choices &optional display-fn)
   (when (featurep 'popup)
     (popup-menu*
-     (mapcar
-      (lambda (choice)
-        (popup-make-item
-         (or (and display-fn (funcall display-fn choice))
-             choice)
-         :value choice))
-      choices)
+     (mapcar (lambda (choice)
+               (popup-make-item 
+                (or (and display-fn (funcall display-fn choice)) choice) 
+                :value choice))
+             choices)
      :prompt prompt
      ;; start isearch mode immediately
-     :isearch t
-     )))
+     :isearch t)))
 
 (setq yas-prompt-functions
       '(yas-popup-isearch-prompt yas-ido-prompt yas-no-prompt))
 
 
 (require-package 'angular-snippets)
-
+(require 'angular-snippets)
 (provide 'init-yasnippet)
 ;;; init-yasnippet.el ends here
