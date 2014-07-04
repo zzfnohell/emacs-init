d9866cebc8f5af15acb094581ae439110c4327b6
Zongfeng Zhang <zzfnohell@gmail.com>
2014-07-04 21:15:25 +0800

    dired

--- init-dired.el
+++ init-dired.el
@@ -1,6 +1,14 @@
+;;; init-dired.el ---
+;;; Code:
+
 ;; set dired mode format,put it before the desktop load
 (setq dired-listing-switches "-avl --time-style=long-iso")
 (require-package 'dired+)
-;;(toggle-diredp-find-file-reuse-dir 1)
+(toggle-diredp-find-file-reuse-dir 1)
+
+;;; Commentary:
+;; 
+
 (provide 'init-dired)
 
+;;; init-dired.el ends here
