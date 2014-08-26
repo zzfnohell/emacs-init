d2c1cef20a8f418ff02ff866e5e682aabecb16f2
Zhang,Zongfeng <zhangzongfeng@xlfz.com>
2014-08-26 09:34:38 +0800

    coding system on windows/cygwin

--- init-coding.el
+++ init-coding.el
@@ -1,3 +1,18 @@
-(prefer-coding-system 'utf-8)
+;;; init-coding.el --- coding system
+
+;;; Commentary:
+;; 
+
+;;; Code:
+
+(cond
+ ((or
+   (eq system-type 'cygwin)
+   (eq system-type 'windows-nt))
+  (prefer-coding-system 'gbk))
+ (t
+  (prefer-coding-system 'utf-8)))
 
 (provide 'init-coding)
+
+;;; init-coding.el ends here
