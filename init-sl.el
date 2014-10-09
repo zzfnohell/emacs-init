c4663b77dcc05487d0e4fc8e617b1d440a1e9797
zzfnohell <zzfnohell@gmail.com>
2014-10-09 09:09:48 +0800

    test hlsl mode available

--- init-sl.el
+++ init-sl.el
@@ -12,9 +12,11 @@
 (add-to-list 'auto-mode-alist '("\\.frag\\'" . glsl-mode))
 (add-to-list 'auto-mode-alist '("\\.geom\\'" . glsl-mode))
 
-(autoload 'hlsl-mode "hlsl-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.fx\\'" . hlsl-mode))
-(add-to-list 'auto-mode-alist '("\\.hlsl\\'" . hlsl-mode))
+	;;(autoload 'hlsl-mode "hlsl-mode" nil t)
+(if (require 'hlsl-mode nil 'noerror)
+	(progn
+	  (add-to-list 'auto-mode-alist '("\\.fx\\'" . hlsl-mode))
+	  (add-to-list 'auto-mode-alist '("\\.hlsl\\'" . hlsl-mode))))
 
 (provide 'init-sl)
 
