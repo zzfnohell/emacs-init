4bd23e4d255e5650ebde19872e4f291be458dbff
zzfnohell <zzfnohell@gmail.com>
2015-03-13 16:20:49 +0000

    disable highlight line

--- custom.el
+++ custom.el
@@ -1,3 +1,10 @@
+;;; custom.el --- custom
+
+
+;;; Commentary:
+;; 
+
+;;; Code:
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -12,3 +19,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;; custom.el ends here
--- init-highlight-line.el
+++ init-highlight-line.el
@@ -1,3 +1,13 @@
-(global-hl-line-mode t)
+;;; init-highlight-line.el --- Hight Line
+
+;;; Commentary:
+;; 
+
+;;; Code:
+
+;; (global-hl-line-mode t)
+
 
 (provide 'init-highlight-line)
+
+;;; init-highlight-line.el ends here
