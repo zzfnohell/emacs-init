f8374c2b06f3e32d87f54ff0042734a8f5ed171f
zzfnohell <zzfnohell@gmail.com>
2015-04-24 12:51:47 +0800

    config for git

--- init-git.el
+++ init-git.el
@@ -8,6 +8,19 @@
 (require 'magit)
 (setq magit-last-seen-setup-instructions "1.4.0")
 
+(require-package 'git-blame)
+(require 'git-blame)
+(autoload 'git-blame-mode "git-blame"
+             "Minor mode for incremental blame for Git." t)
+
+(require-package 'git-gutter)
+(require 'git-gutter)
+;; If you enable global minor mode
+(global-git-gutter-mode t)
+
+;; If you would like to use git-gutter.el and linum-mode
+(git-gutter:linum-setup)
+
 (require-package 'magit-svn)
 (require 'magit-svn)
 
