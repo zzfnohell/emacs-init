fa50961e8044023f866a5a54e196a88533279d18
zzfnohell <zzfnohell@gmail.com>
2015-04-24 13:17:06 +0800

    ac octave

--- init-octave.el
+++ init-octave.el
@@ -18,6 +18,11 @@
 ;;; Commentary:
 ;; require ac-octave
 (require-package 'ac-octave)
+(require 'ac-octave) 
+(defun ac-octave-mode-setup () 
+  (setq ac-sources '(ac-source-octave))) 
+(add-hook 'octave-mode-hook 
+		  '(lambda () (ac-octave-mode-setup))) 
 
 (provide 'init-octave)
 
