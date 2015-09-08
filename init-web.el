b1bdd335adb041f2ee708bfbfae8095199ae6f2a
John Zhang <zzfnohell@gmail.com>
2015-09-08 16:31:17 +0800

    ac-html update

--- init-web.el
+++ init-web.el
@@ -72,23 +72,42 @@
 			 '("css" . (ac-source-css-property
 						ac-source-emmet-css-snippets)))
 
-(add-to-list 'web-mode-ac-sources-alist
-             '("html" . (
-                         ;; attribute-value better to be first
-						 ;; ac-source-words-in-buffer
-						 ;; ac-source-abbrev
-						 ;; ac-source-emmet-html-aliases
-						 ;; ac-source-emmet-html-snippets
-                         ac-source-html-attribute-value
-                         ac-source-html-tag
-                         ac-source-html-attribute)))
+;; (add-to-list 'web-mode-ac-sources-alist
+;;              '("html" . (
+;;                          ;; attribute-value better to be first
+;; 			 ;; ac-source-words-in-buffer
+;; 			 ;; ac-source-abbrev
+;; 			 ;; ac-source-emmet-html-aliases
+;; 			 ;; ac-source-emmet-html-snippets
+;;                          ;;ac-source-html-attribute-value
+;;                          ;;ac-source-html-tag
+;;                          ;;ac-source-html-attribute
+;; 			 )))
 
 (require-package 'ac-html)
-(add-hook 'html-mode-hook 'ac-html-enable)
+(defun setup-ac-for-haml ()
+  ;; Require ac-haml since we are setup haml auto completion
+  (require 'ac-haml)
+  ;; Require default data provider if you want to use
+  (require 'ac-html-default-data-provider)
+  ;; Enable data providers,
+  ;; currently only default data provider available
+  (ac-html-enable-data-provider 'ac-html-default-data-provider)
+  ;; Let ac-haml do some setup
+  (ac-haml-setup)
+  ;; Set your ac-source
+  (setq ac-sources '(ac-source-haml-tag
+                     ac-source-haml-attr
+                     ac-source-haml-attrv))
+  ;; Enable auto complete mode
+  (auto-complete-mode))
 
-(add-hook 'haml-mode-hook 'ac-haml-enable)
-(add-hook 'jade-mode-hook 'ac-jade-enable)
-(add-hook 'slim-mode-hook 'ac-slim-enable)
+(add-hook 'haml-mode-hook 'setup-ac-for-haml)
+
+(add-to-list 'web-mode-ac-sources-alist
+             '("html" . (ac-source-html-tag
+                         ac-source-html-attr
+                         ac-source-html-attrv)))
 
 (require-package 'ac-html-bootstrap)
 (require-package 'ac-html-csswatcher)
