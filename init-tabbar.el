c43c174b675fbce093db952eeb31b217a473f51e
John Zhang <zzfnohell@gmail.com>
2015-10-08 14:19:39 +0800

    use 1 group in tarbar mode

--- init-tabbar.el
+++ init-tabbar.el
@@ -13,16 +13,16 @@
      Return a list of one element based on major mode."
    (list
     (cond
-     ((or (get-buffer-process (current-buffer))
-          ;; Check if the major mode derives from `comint-mode' or
-          ;; `compilation-mode'.
-          (tabbar-buffer-mode-derived-p
-           major-mode 
-		   '(comint-mode compilation-mode)))
-      "Process")
-	 ((string-match-p "\\*.*" (buffer-name))
-	  (cond ((string-match-p "\\*helm.*\\*" (buffer-name)) "Helm")
-			(t "Background")))
+     ;; ((or (get-buffer-process (current-buffer))
+     ;;      ;; Check if the major mode derives from `comint-mode' or
+     ;;      ;; `compilation-mode'.
+     ;;      (tabbar-buffer-mode-derived-p
+     ;;       major-mode 
+	 ;;       '(comint-mode compilation-mode)))
+     ;;  "Process")
+	 ;; ((string-match-p "\\*.*" (buffer-name))
+	 ;;  (cond ((string-match-p "\\*helm.*\\*" (buffer-name)) "Helm")
+	 ;;    	(t "Background")))
 	 (t "Main"))))
 
 (setq tabbar-buffer-groups-function 'tabbar-buffer-groups)
