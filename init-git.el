7a4205ab9e0b8cc06bf72cca0f73e36a709d0636
John Z <zzfnohell@gmail.com>
2015-10-26 14:46:25 +0800

    magit svn

--- init-git.el
+++ init-git.el
@@ -21,6 +21,20 @@
 ;; If you would like to use git-gutter.el and linum-mode
 (git-gutter:linum-setup)
 
+(require-package 'magit-svn)
+;; To enable the mode in a particular repository use:
+;;   cd /path/to/repository
+;;   git config --add magit.extension svn
+
+;; To enable the mode for all repositories use:
+
+;;   git config --global --add magit.extension svn
+
+;; To enable the mode globally without dropping to a shell:
+
+;;   (add-hook 'magit-mode-hook 'magit-svn-mode)
+
+(add-hook 'magit-mode-hook 'magit-svn-mode)
 (provide 'init-git)
 
 ;;; init-git.el ends here
