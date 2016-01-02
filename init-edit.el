c5d785808ac5bdd386d4bd52e9d0db3bc337fd61
zzfnohell <zzfnohell@gmail.com>
2016-01-02 22:10:10 +0800

    vline

--- init-edit.el
+++ init-edit.el
@@ -58,6 +58,19 @@
 
 (require-package 'regex-tool)
 
+(require-package 'vline)
+(require 'vline)
+
+(require-package 'visible-mark)
+(require 'visible-mark)
+(global-visible-mark-mode 1) ;; or add (visible-mark-mode) to specific hooks
+
+(defface visible-mark-active ;; put this before (require 'visible-mark)
+  '((((type tty) (class mono)))
+    (t (:background "magenta"))) "")
+(setq visible-mark-max 2)
+(setq visible-mark-faces `(visible-mark-face1 visible-mark-face2))
+
 (provide 'init-edit)
 
 ;;; init-edit.el ends here
