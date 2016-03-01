effcc3bdc1f93b22c6b332bd4c770aaef3e6d6e7
zzfnohell <zzfnohell@gmail.com>
2016-03-01 21:36:58 +0800

    setup proxy

--- init-proxy.el
+++ init-proxy.el
@@ -5,15 +5,19 @@
 
 ;;; Code:
 
-(defun enable-proxy
-    "Enable proxy."
+(defun enable-proxy ()
+  "Enable proxy."
+  (interactive)
+  (message (format "enable proxy."))
   (setq url-proxy-services
         '(("no_proxy" . "^\\(localhost\\|192.*\\)")
           ("http" . "127.0.0.1:8087")
           ("https" . "127.0.0.1:8087"))))
 
-(defun disable-proxy
-    "Disable proxy."
+(defun disable-proxy ()
+  "Disable proxy."
+  (interactive)
+  (message (format "disable proxy."))
   (setq url-proxy-services nil))
 
 ;; (setq url-http-proxy-basic-auth-storage
