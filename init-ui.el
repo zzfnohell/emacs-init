7a7be689978ea6633423ee1227f24ba87db70536
zzfnohell <zzfnohell@gmail.com>
2016-03-05 10:38:26 +0800

    delete tabbar settings

--- init-ui.el
+++ init-ui.el
@@ -122,28 +122,6 @@
 
 (require-package 'minimap)
 
-
-;; (require 'tabbar)
-
-;; (defun tabbar-buffer-groups ()
-;;   "Return the list of group names the current buffer belongs to.
-;;    Return a list of one element based on major mode."
-;;    (list
-;;     (cond
-;;      ;; ((or (get-buffer-process (current-buffer))
-;;      ;;      ;; Check if the major mode derives from `comint-mode' or
-;;      ;;      ;; `compilation-mode'.
-;;      ;;      (tabbar-buffer-mode-derived-p
-;;      ;;       major-mode 
-;; 	 ;;       '(comint-mode compilation-mode)))
-;;      ;;  "Process")
-;; 	 ;; ((string-match-p "\\*.*" (buffer-name))
-;; 	 ;;  (cond ((string-match-p "\\*helm.*\\*" (buffer-name)) "Helm")
-;; 	 ;;    	(t "Background")))
-;; 	 (t "Main"))))
-
-;; (setq tabbar-buffer-groups-function 'tabbar-buffer-groups)
-
 (provide 'init-ui)
 
 ;;; init-ui.el ends here
