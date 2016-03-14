dbacfcd9d4b1beb050b85d5a4aa4da13f7ed930a
zzfnohell <zzfnohell@gmail.com>
2016-03-14 23:00:38 +0800

    ecb init

--- init-ecb.el
+++ init-ecb.el
@@ -11,13 +11,29 @@
 ;;(setq stack-trace-on-error t)
 (ecb-byte-compile)
 
+
+
+(defvar default-ecb-source-path
+  (list
+   '("~/local" "local")
+   '("~/" "~/")
+   '("/" "/")))
+
+(add-hook 'ecb-basic-buffer-sync-hook
+		  (lambda ()
+			(when (functionp 'projectile-get-project-directories)
+			  (when (projectile-project-p)
+				(dolist (path-dir (projectile-get-project-directories))
+				  (unless (member (list path-dir path-dir) default-ecb-source-path)
+					(push (list path-dir path-dir) default-ecb-source-path)
+					(customize-set-variable 'ecb-source-path default-ecb-source-path)
+					))))))
+
 (setq ecb-auto-activate nil
       ecb-tip-of-the-day nil
       inhibit-startup-message t
       ecb-auto-compatibility-check nil
       ecb-version-check t)
-
-(add-to-list 'ecb-source-path (list *workspace* "default"))
 
 ;; (add-hook 'ecb-history-buffer-after-create-hook 'evil-emacs-state)
 ;; (add-hook 'ecb-directories-buffer-after-create-hook 'evil-emacs-state)
