79bc73e664212b2d8319850055168759eadd3564
Zongfeng Zhang <zzfnohell@gmail.com>
2016-03-15 16:36:24 +0800

    helm key binding

--- init-helm.el
+++ init-helm.el
@@ -37,6 +37,11 @@
 (require-package 'helm-fuzzy-find)
 (require 'helm-fuzzy-find)
 
+(global-set-key (kbd "M-x") 'undefined)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x r b") 'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+
 (helm-mode 1)
 
 (provide 'init-helm)
