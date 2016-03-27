0a0bbb5469db090ac16e6aec548458c504bbce85
zzfnohell <zzfnohell@gmail.com>
2016-03-27 22:53:50 +0800

    iint jedi

--- init-python.el
+++ init-python.el
@@ -19,9 +19,10 @@
 (setq ein:use-smartrep t)
 
 (require-package 'jedi)
+(require 'jedi)
 (add-hook 'python-mode-hook 'jedi:setup)
-;; (setq jedi:setup-keys t)                      ; optional
-;; (setq jedi:complete-on-dot t)                 ; optional
+(setq jedi:setup-keys t)
+(setq jedi:complete-on-dot t)
 
 (require-package 'highlight-indentation)
 (require 'highlight-indentation)
