6503985b22002b3d84b74cd39316510f9eeb93e8
John Zhang <zzfnohell@gmail.com>
2017-07-29 20:00:53 +0800

    ctags update

--- init-tags.el
+++ init-tags.el
@@ -28,20 +28,12 @@
 (use-package ctags-update
   :config
   (progn
-    (autoload
-      'turn-on-ctags-auto-update-mode
-      "ctags-update"
-      "turn on `ctags-auto-update-mode'." t)
-    (add-hook
-     'c-mode-common-hook
-     'turn-on-ctags-auto-update-mode)
-
-    (add-hook
-     'emacs-lisp-mode-hook
-     'turn-on-ctags-auto-update-mode)
-
+    (ctags-global-auto-update-mode)
+    (setq ctags-update-prompt-create-tags nil)
+    (autoload 'turn-on-ctags-auto-update-mode "ctags-update" "turn on `ctags-auto-update-mode'." t)
+    (add-hook 'c-mode-common-hook 'turn-on-ctags-auto-update-mode)
+    (add-hook 'emacs-lisp-mode-hook 'turn-on-ctags-auto-update-mode)
     (autoload 'ctags-update "ctags-update" "update TAGS using ctags" t)
-
     )
   )
 
