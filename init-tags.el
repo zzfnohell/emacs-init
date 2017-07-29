1a27540dcc3b9bad8e699788a6adc81209c72b98
John Zhang <zzfnohell@gmail.com>
2017-07-29 19:09:20 +0800

    tags

--- init-tags.el
+++ init-tags.el
@@ -5,36 +5,46 @@
 
 ;;; Code:
 
-(require-package 'ggtags)
-(autoload 'gtags-mode "gtags" "" t)
+(use-package ggtags
+  :config
+  (progn
+    (autoload 'gtags-mode "gtags" "" t)
 
-(add-hook 'c-mode-common-hook
-          '(lambda ()
-             (gtags-mode 1)))
-;; [Setting to make 'Gtags select mode' easy to see]
-(add-hook
- 'gtags-select-mode-hook
- '(lambda ()
-    (setq hl-line-face
-          '((t (:bold t :foreground "blue" :weight extra-bold)
-               underline)))
-    (hl-line-mode 1)))
+    (add-hook 'c-mode-common-hook
+              '(lambda ()
+                 (gtags-mode 1)))
+    ;; [Setting to make 'Gtags select mode' easy to see]
+    (add-hook
+     'gtags-select-mode-hook
+     '(lambda ()
+        (setq hl-line-face
+              '((t (:bold t :foreground "blue" :weight extra-bold)
+                   underline)))
+        (hl-line-mode 1)))
 
-(require-package 'ctags-update)
-(autoload
-  'turn-on-ctags-auto-update-mode
-  "ctags-update"
-  "turn on
+    )
+  )
+
+(use-package ctags-update
+  :config
+  (progn
+    (autoload
+      'turn-on-ctags-auto-update-mode
+      "ctags-update"
+      "turn on
  `ctags-auto-update-mode'." t)
-(add-hook
- 'c-mode-common-hook
- 'turn-on-ctags-auto-update-mode)
+    (add-hook
+     'c-mode-common-hook
+     'turn-on-ctags-auto-update-mode)
 
-(add-hook
- 'emacs-lisp-mode-hook
- 'turn-on-ctags-auto-update-mode)
+    (add-hook
+     'emacs-lisp-mode-hook
+     'turn-on-ctags-auto-update-mode)
 
-(autoload 'ctags-update "ctags-update" "update TAGS using ctags" t)
+    (autoload 'ctags-update "ctags-update" "update TAGS using ctags" t)
+
+    )
+  )
 
 (provide 'init-tags)
 
