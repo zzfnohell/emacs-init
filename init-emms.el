30de38ff546b094bd289d9db992e5e60c721ea3e
John Zhang <zzfnohell@gmail.com>
2017-07-29 19:03:37 +0800

    java

--- init-emms.el
+++
@@ -1,7 +0,0 @@
-
-(cond
- ((eq system-type 'gnu/linux)
-  (require-package 'emms)
-  (require 'emms-setup)
-  (emms-default-players)))
-(provide 'init-emms)
--- init-java.el
+++ init-java.el
@@ -8,28 +8,18 @@
 
 (require 'cedet)
 (require 'semantic)
-(require-package 'groovy-mode)
-;;(require-package 'malabar-mode)
-;;(require 'malabar-mode)
-;;(setq malabar-groovy-lib-dir "/path/to/malabar/lib")
-;;(add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
-
-(require-package 'java-snippets)
-(require-package 'javadoc-lookup)
+(use-package groovy-mode)
+(use-package java-snippets)
+(use-package javadoc-lookup)
 (require 'javadoc-lookup)
 
 (setq open-jdk-doc-path "/usr/share/doc/openjdk-7-jdk/api")
 (if (file-exists-p open-jdk-doc-path)
     (javadoc-add-roots open-jdk-doc-path))
 
-(require-package 'ant)
-(require 'ant)
-(require-package 'mvn)
-(require 'mvn)
-
-(require-package 'ant)
-
-(require-package 'jdee)
+(use-package ant)
+(use-package mvn)
+(use-package jdee)
 
 (provide 'init-java)
 ;;; init-java.el ends here
