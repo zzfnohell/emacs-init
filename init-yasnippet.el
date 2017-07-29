9529020fdd8305406fcc4b86e4d440264e352544
John Zhang <zzfnohell@gmail.com>
2017-07-29 19:18:57 +0800

    yas

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -4,44 +4,42 @@
 ;; YASnippet
 
 ;;; Code:
-(require-package 'yasnippet)
-(require 'yasnippet)
 
-(add-to-list 'yas-snippet-dirs 
-			 "~/.emacs.d/snippets"
-			 "~/.emacs.d/snippets/yasmate/snippets")
 
-(define-key yas-minor-mode-map [(tab)] nil)
-(define-key yas-minor-mode-map (kbd "TAB") nil)
+(use-package yasnippet
+  :config
+  (progn
+    ;;; use popup menu for yas-choose-value
+    (require 'popup)
 
-(yas-reload-all)
-(yas-global-mode 1)
+    (add-to-list 'yas-snippet-dirs 
+                 "~/.emacs.d/snippets"
+                 "~/.emacs.d/snippets/yasmate/snippets")
 
-;;; use popup menu for yas-choose-value
-(require 'popup)
+    (define-key yas-minor-mode-map [(tab)] nil)
+    (define-key yas-minor-mode-map (kbd "TAB") nil)
 
-(defun yas-popup-isearch-prompt (prompt choices &optional display-fn)
-  (when (featurep 'popup)
-    (popup-menu*
-     (mapcar (lambda (choice)
-               (popup-make-item 
-                (or (and display-fn (funcall display-fn choice)) choice) 
-                :value choice))
-             choices)
-     :prompt prompt
-     ;; start isearch mode immediately
-     :isearch t)))
+    (yas-reload-all)
+    (yas-global-mode 1)
 
-(setq yas-prompt-functions
-      '(yas-popup-isearch-prompt
-		yas-ido-prompt
-		yas-no-prompt))
+    (defun yas-popup-isearch-prompt (prompt choices &optional display-fn)
+      (when (featurep 'popup)
+        (popup-menu*
+         (mapcar (lambda (choice)
+                   (popup-make-item 
+                    (or (and display-fn (funcall display-fn choice)) choice) 
+                    :value choice))
+                 choices)
+         :prompt prompt
+         ;; start isearch mode immediately
+         :isearch t)))
 
-(require-package 'angular-snippets)
-(require 'angular-snippets)
+    (setq yas-prompt-functions
+          '(yas-popup-isearch-prompt
+            yas-ido-prompt
+            yas-no-prompt))))
 
-(require-package 'auto-yasnippet)
-(require 'auto-yasnippet)
+(use-package auto-yasnippet)
 
 (provide 'init-yasnippet)
 
