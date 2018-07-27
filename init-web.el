78357b81a44f06cc8d4c66aba44341062b54f197
Zongfeng Zhang <zhangzongfeng@xlfz.cn>
2018-07-27 12:55:30 +0800

    x

--- init-web.el
+++ init-web.el
@@ -5,72 +5,74 @@
 
 ;;; Code:
 
+(defun init-web/web-mode/custom-web-mode-hook ()
+  "Hooks for Web mode."
+	;; Syntax Higlighting
+	(set-face-attribute 'web-mode-css-rule-face nil :foreground "Pink3")
+
+	;; Auto-pairs
+  (setq web-mode-extra-auto-pairs
+        '(("erb"  . (("beg" "end")))
+          ("php"  . (("beg" "end")
+                     ("beg" "end")))))
+	
+	;; Enable / disable Features
+  (setq web-mode-enable-auto-pairing t)
+  (setq web-mode-enable-css-colorization t)
+  (setq web-mode-enable-block-face t)
+  (setq web-mode-enable-part-face t)
+  (setq web-mode-enable-comment-keywords t)
+  (setq web-mode-enable-heredoc-fontification t)
+
+	;; Current element / column highlight
+	(setq web-mode-enable-current-element-highlight t)
+  (setq web-mode-enable-current-column-highlight t)
+
+	;; Context-aware auto-completion
+	(setq web-mode-ac-sources-alist
+  '(("php" . (ac-source-yasnippet ac-source-php-auto-yasnippets))
+    ("html" . (ac-source-emmet-html-aliases ac-source-emmet-html-snippets))
+    ("css" . (ac-source-css-property ac-source-emmet-css-snippets))))
+
+	(add-hook 'web-mode-before-auto-complete-hooks
+						'(lambda ()
+							 (let ((web-mode-cur-language
+											(web-mode-language-at-pos)))
+								 (if (string= web-mode-cur-language "php")
+										 (yas-activate-extra-mode 'php-mode)
+									 (yas-deactivate-extra-mode 'php-mode))
+								 (if (string= web-mode-cur-language "css")
+										 (setq emmet-use-css-transform t)
+									 (setq emmet-use-css-transform nil)))))
+	
+	;; company-web
+	(define-key web-mode-map (kbd "C-'") 'company-web-html)
+	(add-hook 'web-mode-hook
+						(lambda ()
+							(set (make-local-variable 'company-backends)
+									 '(company-web-html company-files))
+							(company-mode t)))
+	)
+
+
 (req-package web-mode
-	:require company-web
+	:demand t
   :config
-  (progn
-    (setq web-mode-enable-current-element-highlight nil)
-    (setq web-mode-enable-current-column-highlight nil)
+  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
-    (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-
-    (setq web-mode-engines-alist
-          '(("php"    . "\\.phtml\\'")
-            ("blade"  . "\\.blade\\.")))
-
-    (add-to-list 'auto-mode-alist '("\\.api\\'" . web-mode))
-
-    (defun init-web/custom-web-mode-hook ()
-      "Hooks for Web mode."
-      (setq web-mode-markup-indent-offset 2)
-      (setq web-mode-css-indent-offset 2)
-      (setq web-mode-code-indent-offset 2)
-      (add-to-list 'web-mode-indentation-params '("lineup-args" . nil))
-      (add-to-list 'web-mode-indentation-params '("lineup-calls" . nil))
-      (add-to-list 'web-mode-indentation-params '("lineup-concats" . nil))
-      (setq web-mode-style-padding 1)
-      (setq web-mode-script-padding 1)
-      (setq web-mode-block-padding 0)
-      (setq web-mode-comment-style 2)
-      (setq web-mode-extra-auto-pairs
-            '(("erb"  . (("beg" "end")))
-              ("php"  . (("beg" "end")
-                         ("beg" "end")))))
-      (setq web-mode-enable-auto-pairing t)
-      (setq web-mode-enable-css-colorization t)
-      (setq web-mode-enable-block-face t)
-      (setq web-mode-enable-part-face t)
-      (setq web-mode-enable-comment-keywords t)
-      (setq web-mode-enable-heredoc-fontification t)
-
-			;; company-web
-			(define-key web-mode-map (kbd "C-'") 'company-web-html)
-			(add-hook 'web-mode-hook
-								(lambda ()
-									(set (make-local-variable 'company-backends)
-											 '(company-web-html company-files))
-									(company-mode t)))
-			)
-
-    (add-hook 'web-mode-hook  'init-web/custom-web-mode-hook)
-    (add-hook 'web-mode-before-auto-complete-hooks
-              '(lambda ()
-                 (let ((web-mode-cur-language
-                        (web-mode-language-at-pos)))
-                   (if (string= web-mode-cur-language "php")
-                       (yas-activate-extra-mode 'php-mode)
-                     (yas-deactivate-extra-mode 'php-mode))
-                   (if (string= web-mode-cur-language "css")
-                       (setq emmet-use-css-transform t)
-                     (setq emmet-use-css-transform nil)))))
-    )
+	(setq web-mode-engines-alist
+				'(("php"    . "\\.phtml\\'")
+					("blade"  . "\\.blade\\."))
+				)
+	
+  (add-hook 'web-mode-hook  'init-web/web-mode/custom-web-mode-hook)
   )
 
 (use-package ac-html-bootstrap)
