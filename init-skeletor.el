2294ed2aed0489f31e61db01d425be339b66a0cb
Zongfeng Zhang <zhangzongfeng@xlfz.cn>
2018-08-01 11:37:54 +0800

    skewer index skeletor project

--- init-skeletor.el
+++ init-skeletor.el
@@ -8,6 +8,13 @@
 	:defer t
 	:custom
 	(skeletor-user-directory "~/.emacs.d/init/init-skeletor-projects")
+	:config
+	(skeletor-define-template "skewer-index-html-package"
+		:title "Skewer Index Html"
+		:no-license? t
+		:no-git? t
+		:substitutions
+		'(("__TITLE__" . (lambda () (read-string "Description: ")))))
 	)
 
 (provide 'init-skeletor)
