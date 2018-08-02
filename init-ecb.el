d4e19d12454ea8eec1d1e8457c0689def60a184c
Zongfeng Zhang <zhangzongfeng@xlfz.cn>
2018-08-02 16:57:22 +0800

    projectile

--- init-ecb.el
+++ init-ecb.el
@@ -8,38 +8,13 @@
 (use-package ecb
 	:ensure t
   :defer t
+	:custom
+	(ecb-windows-width 30)
+  (ecb-source-path '(("~"  "home")))
   :config
-  (progn
-    ;;(setq stack-trace-on-error t)
-    (ecb-byte-compile)
-
-    (defvar default-ecb-source-path
-      (list
-       '("~/" "~/")
-       '("/" "/")
-       ))
-
-    (add-hook 'ecb-basic-buffer-sync-hook
-              (lambda ()
-                (when (functionp 'projectile-get-project-directories)
-                  (when (projectile-project-p)
-                    (dolist (path-dir (projectile-get-project-directories))
-                      (unless (member (list path-dir path-dir) default-ecb-source-path)
-                        (push (list path-dir path-dir) default-ecb-source-path)
-                        (customize-set-variable 'ecb-source-path default-ecb-source-path)
-                        ))))))
-
-    (setq ecb-auto-activate nil
-          ecb-tip-of-the-day nil
-          inhibit-startup-message t
-          ecb-auto-compatibility-check nil
-          ecb-version-check t)
-	;; (add-hook 'ecb-history-buffer-after-create-hook 'evil-emacs-state)
-    ;; (add-hook 'ecb-directories-buffer-after-create-hook 'evil-emacs-state)
-    ;; (add-hook 'ecb-methods-buffer-after-create-hook 'evil-emacs-state)
-    ;; (add-hook 'ecb-sources-buffer-after-create-hook 'evil-emacs-state)
-
-	(custom-set-variables'(ecb-windows-width 30)))
+  ;;(setq stack-trace-on-error t)
+  (ecb-byte-compile)
+  (setq ecb-auto-activate nil)
 )
 
 
