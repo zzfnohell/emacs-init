f9a27581d7534474f521b4576f1c02273b896f7b
Zongfeng Zhang <zhangzongfeng@xlfz.cn>
2018-08-06 16:35:23 +0800

    ml

--- init-ml.el
+++ init-ml.el
@@ -2,60 +2,45 @@
 
 ;;; Commentary:
 ;;
-(require 'nxml-mode)
-(fset 'xml-mode 'nxml-mode)
-(add-to-list 'auto-mode-alist
-						 (cons 
-							(concat "\\." 
-											(regexp-opt '("xml" 
-																		"xsd"
-																		"sch"
-																		"rng" 
-																		"xslt" 
-																		"svg"
-																		"rss") t)
-											"\\'")
-							'nxml-mode))
 
-;;; Use nxml-mode instead of sgml, xml or html mode.
-(mapc (lambda (pair)
-				(if (or (eq (cdr pair) 'xml-mode)
-								(eq (cdr pair) 'sgml-mode))
-						(setcdr pair 'nxml-mode)))
-			magic-mode-alist)
+(use-package nxml-mode
+  :ensure t
+  :mode (("\\.xml\\'" . nxml-mode)
+         ("\\.xsd\\'" . nxml-mode)
+         ("\\.sch\\'" . nxml-mode)
+         ("\\.rng\\'" . nxml-mode)
+         ("\\.xslt\\'" . nxml-mode)
+         ("\\.svg\\'" . nxml-mode)
+         ("\\.rss\\'" . nxml-mode))
+  )
 
 ;; See: http://sinewalker.wordpress.com/2008/06/26/pretty-printing-xml-with-emacs-nxml-mode/
 (defun pp-xml-region (begin end)
-	"Pretty format XML markup in region. The function inserts
+  "Pretty format XML markup in region. The function inserts
 linebreaks to separate tags that have nothing but whitespace
 between them.  It then indents the markup by using nxml's
 indentation rules."
-	(interactive "r")
-	(save-excursion
+  (interactive "r")
+  (save-excursion
     (nxml-mode)
     (goto-char begin)
     (while (search-forward-regexp "\>[ \\t]*\<" nil t)
       (backward-char) (insert "\n"))
     (indent-region begin end)))
 
-(use-package markdown-mode
-	:ensure t
-	)
+(use-package markdown-mode :ensure t)
 
-(use-package auto-complete-nxml
-	:ensure t
-	)
+(use-package auto-complete-nxml :ensure t)
 
 (use-package haml-mode
-	:after markdown-mode
-	:ensure t
-	)
+  :after markdown-mode
+  :ensure t
+  )
 
 ;;; YAML
 (use-package yaml-mode
-	:ensure t
-	
-  :config (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+  :ensure t
+  :mode ("\\.yml$" . yaml-mode)
   )
 
 (provide 'init-ml)
