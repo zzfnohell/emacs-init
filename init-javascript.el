1ae443de8c29efddabc9a68f78d1d29d7098f4e7
Zongfeng Zhang <zhangzongfeng@xlfz.cn>
2018-08-07 16:22:05 +0800

    flow for emacs

--- init-javascript.el
+++ init-javascript.el
@@ -68,7 +68,7 @@
 ;;               (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
 ;;   )
 
-(use-package rjsx-mode :ensure t)
+
 
 (use-package tern
   :ensure t
@@ -132,14 +132,19 @@
   :mode ("\\.jade$" . jade-mode)
   )
 
+;; (use-package rjsx-mode :ensure t)
+
 ;; npm install -g flow-language-server
 (use-package lsp-javascript-flow
   :after lsp-mode
 	:ensure t
 	:config
 	(add-hook 'js-mode-hook #'lsp-javascript-flow-enable)
-	(add-hook 'rjsx-mode #'lsp-javascript-flow-enable) ;; for rjsx-mode support
+  ;; for rjsx-mode support
+  ;; (add-hook 'rjsx-mode #'lsp-javascript-flow-enable)
 	)
+
+(load-file "~/.emacs.d/init/site-lisp/flow-for-emacs/flow.el")
 
 (use-package indium :ensure t)
 
