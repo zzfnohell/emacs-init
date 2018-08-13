2941bd125e0cd8f80e123c2c85b34ac63fcf7933
Zongfeng Zhang <zzfnohell@outlook.com>
2018-08-13 21:50:09 +0800

    init ui

--- init-ui.el
+++ init-ui.el
@@ -22,9 +22,9 @@
        (font-spec :family name :size size)))))
 
 (if (not (eq nil window-system))
-    (cond ((eq 'darwin system-type) (set-font "Monaco" 10))
-          ((eq 'gnu/linux system-type) (set-font "Monaco" 10))
-          (t (set-font "Ubuntu Mono" 10))))
+    (cond ((eq 'darwin system-type) (set-font "Monaco" 12))
+          ((eq 'gnu/linux system-type) (set-font "Monaco" 12))
+          (t (set-font "Ubuntu Mono" 12))))
 
 ;; emacs shell font confusion
 (defvar ansi-color-for-comint-mode t)
