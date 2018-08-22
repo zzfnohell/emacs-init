db4369d6f978d4e478153310c1bd883796246d36
Zongfeng Zhang <zhangzongfeng@xlfz.cn>
2018-08-22 16:31:16 +0800

    magit performance

--- init-vc.el
+++ init-vc.el
@@ -13,7 +13,10 @@
 ;; (require 'ewoc)
 ;; (require 'vc-annotate)
 
-(use-package magit :ensure t)
+(use-package magit
+	:ensure t
+	:config
+	(setq magit-refresh-status-buffer nil))
 
 (provide 'init-vc)
 ;;; init-vc.el ends here
