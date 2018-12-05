ace7d7a82831d3c2c0766dbf0839b07f1773fa0d
Zongfeng Zhang <zhangzongfeng@xlfz.cn>
2018-12-05 11:02:53 +0800

    coding system utf8

--- init-coding.el
+++ init-coding.el
@@ -5,6 +5,8 @@
 
 ;;; Code:
 
+(define-coding-system-alias 'cp65001 'utf-8)
+
 ;; disable CJK coding/encoding (Chinese/Japanese/Korean characters)
 (setq utf-translate-cjk-mode nil) 
 (set-language-environment 'utf-8)
@@ -14,11 +16,9 @@
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 
-;; (set-default-coding-systems 'gbk)
-;; (set-terminal-coding-system 'gbk)
-
-
 (unless (eq system-type 'windows-nt)
+  ;; (set-default-coding-systems 'gbk)
+  ;; (set-terminal-coding-system 'gbk)
 	(set-selection-coding-system 'utf-8))
 
 (provide 'init-coding)
