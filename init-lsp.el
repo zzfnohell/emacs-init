5ac83379376df19078733741e4367c3b8ab4e835
Zongfeng Zhang <zzfnohell@gmail.com>
2019-01-04 23:51:48 +0800

    disable ensure system package lsp

--- init-lsp.el
+++ init-lsp.el
@@ -6,14 +6,15 @@
 ;;; Code:
 (use-package lsp-mode
   :commands lsp
-  :ensure-system-package
-  ((bash-language-server . "npm i -g bash-language-server")
-   (vscode-html-languageserver-bin . "npm i -g vscode-html-languageserver-bin")
-   (vscode-css-languageserver-bin . "npm i -g vscode-css-languageserver-bin")
-   (javascript-typescript-langserver . "npm i -g javascript-typescript-langserver")
-   (go-langserver . "go get -u github.com/sourcegraph/go-langserver")
-   (ocaml-language-server . "npm i -g ocaml-language-server")
-   (python-language-server . "pip install \"python-language-server[all]\"")))
+  ;; :ensure-system-package
+  ;; ((bash-language-server . "npm i -g bash-language-server")
+  ;;  (vscode-html-languageserver-bin . "npm i -g vscode-html-languageserver-bin")
+  ;;  (vscode-css-languageserver-bin . "npm i -g vscode-css-languageserver-bin")
+  ;;  (javascript-typescript-langserver . "npm i -g javascript-typescript-langserver")
+  ;;  (go-langserver . "go get -u github.com/sourcegraph/go-langserver")
+  ;;  (ocaml-language-server . "npm i -g ocaml-language-server")
+  ;;  (python-language-server . "pip install \"python-language-server[all]\""))
+  )
 
 (use-package lsp-ui :commands lsp-ui-mode)
 (use-package company-lsp :commands company-lsp)
