19cd7c6378f7d6447dd81d84bf476421b7dbdbc2
Zongfeng Zhang <zzfnohell@gmail.com>
2019-06-24 23:55:29 +0800

    php mode

--- init-php.el
+++ init-php.el
@@ -3,27 +3,21 @@
 ;;; Commentary:
 ;; 
 
-(use-package php-mode
-  :ensure t
-	)
+(use-package php-mode :ensure t)
 
 (use-package company-php
   :ensure t
-  
   :config
   (add-hook 'php-mode-hook
             '(lambda ()
                (require 'company-php)
                (company-mode t)
-               (add-to-list 'company-backends 'company-ac-php-backend)))
-  )
+               (add-to-list 'company-backends 'company-ac-php-backend))))
 
 (use-package lsp-php
 	:ensure t
-  
 	:config
-	(add-hook 'php-mode-hook #'lsp-php-enable)
-	)
+	(add-hook 'php-mode-hook #'lsp-php-enable))
 
 (provide 'init-php)
 
