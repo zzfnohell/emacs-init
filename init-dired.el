91cbb5104d3851a99131e205d573ab643cbfee8a
Zongfeng Zhang <zhangzongfeng@xlfz.cn>
2019-09-18 11:48:57 +0800

    dired use ls-lisp

--- init-dired.el
+++ init-dired.el
@@ -4,12 +4,7 @@
 ;;; Commentary:
 ;;
 
-;; set dired mode format,put it before the desktop load
-
-(cond
- ((eq 'windows-nt system-type) nil)
- ((eq 'darwin system-type) nil)
- (t (setq dired-listing-switches "-avl --time-style=long-iso")))
+(require 'ls-lisp)
 
 (provide 'init-dired)
 
