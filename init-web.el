03e903aa6e8130d6a0ffd0e172858bca2af00fde
Zhang Zongfeng <zzfnohell@gmail.com>
2020-02-22 12:54:31 +0800

    emmet mode

--- init-web.el
+++ init-web.el
@@ -57,6 +57,11 @@
                    '(company-web-html company-files))
               (company-mode t))))
 
+(use-package emmet-mode
+  :config
+  (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+  (add-hook 'html-mode-hook 'emmet-mode)
+  (add-hook 'css-mode-hook  'emmet-mode))
 
 (use-package web-mode
   :mode (
