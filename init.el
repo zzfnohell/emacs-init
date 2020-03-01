d512421e5c4f2fc89c4425120429cee391565ca0
Zhang Zongfeng <zzfnohell@gmail.com>
2020-03-01 22:50:07 +0800

    init custom file location

--- init.el
+++ init.el
@@ -10,16 +10,17 @@
 ;; (setq debug-on-message ".*ad-handle-definition.*")
 ;; (setq debug-on-message ".*error.*")
 
+(setq custom-file "~/.emacs.d/custom.el")
+
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-(setq custom-file "~/.emacs.d/custom.el")
+(load "~/.emacs.d/init/init-main.el")
+
 (when (file-exists-p custom-file)
   (load custom-file))
-
-(load "~/.emacs.d/init/init-main.el")
 
 ;;; init.el ends here
