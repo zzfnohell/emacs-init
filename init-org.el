9704e1eab56a0e21ac194fb8d67acda3ba0211f5
Zhang Zongfeng <zzfnohell@gmail.com>
2020-03-02 13:44:46 +0800

    agenda files

--- init-org.el
+++ init-org.el
@@ -4,10 +4,11 @@
 ;; 
 
 ;;; Code:
-(require 'org-install)
 
-(defvar *org-root-directory* "~/OneDrive/Notes/")
-(defvar *org-publish-directory* "~/OneDrive/Note-Publishes/")
+(use-package org-preview-html)
+
+(defvar *org-root-directory* "~/notes/")
+(defvar *org-publish-directory* "~/notes-public/")
 
 (unless (file-directory-p *org-root-directory*)
   (make-directory *org-root-directory* t))
@@ -16,56 +17,23 @@
   (make-directory *org-publish-directory* t))
 
 (setq org-directory *org-root-directory*)
-(setq org-default-notes-file "~/OneDrive/Notes/default.org")
 
-;; capturing
-(setq org-capture-templates
-      '(
-        ("t" "Todo" entry (file+headline "inbox.org" "Tasks")
-         "* TODO %?\n  %i\n  %u\n  %a")
-        ("n" "Note/Data" entry (file+headline "inbox.org" "Notes/Data")
-         "* %?   \n  %i\n  %u\n  %a")
-        ("j" "Journal" entry (file+datetree "~/OneDrive/Notes/journal.org")
-         "* %?\nEntered on %U\n %i\n %a")
-        ))
-(setq org-irc-link-to-logs t)
-
-
-;; link uuid
-(require 'org-id)
-(setq org-id-link-to-org-use-id 'create-if-interactive)
-
-;; time
-(setq org-log-done 'time)
-
-;; agenda
-(setq org-agenda-files (list "~/OneDrive/Notes/agenda.org"))
+(setq org-agenda-files (list "~/notes/agenda.org"))
+(setq org-default-notes-file "~/notes/default.org")
 
 (setq org-publish-project-alist
-      `(
-		    ("note-org"
-		     :base-directory ,*org-root-directory*
-		     :publishing-directory ,*org-publish-directory*
-		     :base-extension "org"
-		     :recursive t
-		     :publishing-function org-publish-org-to-html
-		     :auto-index t
-		     :index-filename "index.org"
-		     :index-title "index"
-		     :link-home "index.html"
-		     :section-numbers nil)
-		    ("note-static"
-		     :base-directory ,*org-root-directory*
-		     :publishing-directory ,*org-publish-directory*
-		     :recursive t
-		     :base-extension "css//|js//|png//|jpg//|gif//|pdf//|mp3//|swf//|zip//|gz//|txt//|el"
-		     :publishing-function org-publish-attachment)
-		    ("note"
-		     :components ("note-org" "note-static")
-		     :author "zzfnohell@gmail.com" )))
-
-(use-package org-preview-html)
-
+      `(("blog-notes"
+         :base-directory *org-root-directory*
+         :recursive t
+         :publishing-directory *org-publish-directory*
+         :publishing-function org-html-publish-to-html)
+        ("blog-static"
+         :base-directory *org-root-directory*
+         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+         :publishing-directory *org-publish-directory*
+         :recursive 
+         :publishing-function org-publish-attachment)
+        ("blog" :components ("blog-notes" "blog-static"))))
 
 (provide 'init-org)
 
