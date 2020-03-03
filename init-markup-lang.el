84675a0d055ed7c34b730831dc8b9f9363b6b0d6
Zhang Zongfeng <zzfnohell@gmail.com>
2020-03-03 14:12:30 +0800

    markup lang

--- init-markup-lang.el
+++ init-markup-lang.el
@@ -16,20 +16,6 @@
 (use-package ini-mode
   :mode (("\\.ini\\'" . ini-mode)))
 
-;; See: http://sinewalker.wordpress.com/2008/06/26/pretty-printing-xml-with-emacs-nxml-mode/
-(defun pp-xml-region (begin end)
-  "Pretty format XML markup in region. The function inserts
-linebreaks to separate tags that have nothing but whitespace
-between them.  It then indents the markup by using nxml's
-indentation rules."
-  (interactive "r")
-  (save-excursion
-    (nxml-mode)
-    (goto-char begin)
-    (while (search-forward-regexp "\>[ \\t]*\<" nil t)
-      (backward-char) (insert "\n"))
-    (indent-region begin end)))
-
 (use-package haml-mode :after markdown-mode)
 
 ;;; YAML
