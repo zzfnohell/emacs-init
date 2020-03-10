4c6231a6f19fc3281442f11eba32aa5a4db7b730
Zhang Zongfeng <zzfnohell@gmail.com>
2020-03-10 11:23:53 +0800

    org custom

--- init-org.el
+++ init-org.el
@@ -9,17 +9,6 @@
 (use-package org-preview-html)
 (use-package ob-axiom)
 
-(defvar *org-root-directory* "~/notes/")
-(defvar *org-publish-directory* "~/notes-public/")
-
-(unless (file-directory-p *org-root-directory*)
-  (make-directory *org-root-directory* t))
-
-(unless (file-directory-p *org-root-directory*)
-  (make-directory *org-publish-directory* t))
-
-(setq org-directory *org-root-directory*)
-
 (use-package org
   :after (:all ob-axiom)
   :config
@@ -48,23 +37,20 @@
      (ruby . t)
      (scheme . t)
      (shell . t)))
-  
-  (setq org-agenda-files (list "~/notes/agenda.org"))
-  (setq org-default-notes-file "~/notes/default.org")
 
   (setq org-publish-project-alist
-      `(("blog-notes"
-         :base-directory ,*org-root-directory*
+      `(("private-notes"
+         :base-directory ,org-directory
          :recursive t
-         :publishing-directory ,*org-publish-directory*
+         :publishing-directory "~/notes-public"
          :publishing-function org-html-publish-to-html)
-        ("blog-static"
-         :base-directory ,*org-root-directory*
+        ("private-static"
+         :base-directory ,org-directory
          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-         :publishing-directory ,*org-publish-directory*
-         :recursive 
+         :publishing-directory "~/notes-public"
+         :recursive t
          :publishing-function org-publish-attachment)
-        ("blog" :components ("blog-notes" "blog-static")))))
+        ("private" :components ("private-notes" "private-static")))))
 
 (provide 'init-org)
 
