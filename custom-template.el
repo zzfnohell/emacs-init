aa6221fe01f83776d7819a96ebc89f8a4d6c1d7d
Zongfeng Zhang <zzfnohell@gmail.com>
2020-04-29 16:09:49 +0800

    custom

--- custom-template.el
+++ custom-template.el
@@ -20,7 +20,7 @@
  '(org-agenda-files (quote ("e:/OneDrive/org/agenda.org")))
  '(org-default-notes-file "e:/OneDrive/org/notes.org")
  '(org-directory "e:/OneDrive/org")
- '(org-mobile-directory "/ssh:/zzfnohell/pluto:/var/local/dav")
+ '(org-mobile-directory "/ssh:zzfnohell@pluto:/var/local/dav")
  '(org-mobile-files
    (quote
     (org-agenda-files "oil.org" "stock.org" "oanda.org" "us_stock.org")))
