ac9759b1c28c68ae33670bd860e13ca32982136e
Zongfeng Zhang <zongfeng.zhang@nufront.com>
2020-09-16 16:46:08 +0800

    delete root project from init.el

--- init.el
+++ init.el
@@ -17,9 +17,4 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
-(ede-cpp-root-project "MINGW64"
-                      :file "~/.emacs.d/init.el"
-                      :include-path '("/include" "../include")
-                      :system-include-path '( "d:/msys64/mingw64/include/" ))
-
 ;;; init.el ends here
