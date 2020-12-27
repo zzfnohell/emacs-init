051bea781540ad633840d55d370701d42cc377e6
John <zzfnohell@gmail.com>
2020-12-27 21:00:29 +0800

    python

--- init-python.el
+++ init-python.el
@@ -4,20 +4,13 @@
 ;; elpy
 
 ;;; Code:
-
-
-(use-package anaconda-mode
-  :config
-  (add-hook 'python-mode-hook 'anaconda-mode)
-  (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
-
 (use-package python-mode
   ;; (add-to-list 'python-shell-extra-pythonpaths "/path/to/the/project")
   ;; (add-to-list 'python-shell-extra-pythonpaths "/path/to/the/dependency")
   )
 
 (use-package ein
-  :config (setq ein:use-smartrep t))
+	:ensure t)
 
 ;; (use-package jedi
 ;;   :config
@@ -33,22 +26,31 @@
 ;; 	(setq elpy-rpc-backend "jedi"))
 
 (use-package highlight-indentation
+	:ensure t
   :config
 	(set-face-background 'highlight-indentation-face "#e3e3d3")
   (set-face-background 'highlight-indentation-current-column-face "#c3b3b3"))
 
 (use-package pyvenv
+	:ensure t
   :config
   (pyvenv-mode 1)
   (pyvenv-tracking-mode 1))
 
 (use-package yapfify
+	:ensure t
   :config (add-hook 'python-mode-hook 'yapf-mode))
 
-(use-package cython-mode)
+(use-package cython-mode
+	:ensure t)
 
-(use-package flycheck-cython)
-(use-package pyenv-mode)
+(use-package flycheck-cython
+	:ensure t)
+
+(use-package pyenv-mode
+	:ensure t
+	:config
+	(pyenv-mode))
 
 (provide 'init-python)
 ;;; init-python.el ends here
