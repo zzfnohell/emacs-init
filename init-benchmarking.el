86eb45104f92d39d514ad39948e3b3bedb085f79
John <zzfnohell@gmail.com>
2021-01-17 17:50:27 +0800

    benchmark

--- init-benchmarking.el
+++ init-benchmarking.el
@@ -5,8 +5,11 @@
 
 ;;; Code:
 
-(use-package esup :defer t)
+(use-package esup
+	:ensure t)
+
 (use-package benchmark-init
+	:ensure t
   :config
   ;; To disable collection of benchmark data after init is done.
   (add-hook 'after-init-hook 'benchmark-init/deactivate))
