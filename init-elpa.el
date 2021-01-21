2b8ff38ddb74ec2eb0f57e6cf83a1084ecdc3aa0
Zongfeng Zhang <zongfeng.zhang@nufront.com>
2021-01-21 14:30:12 +0800

    elpa

--- init-elpa.el
+++ init-elpa.el
@@ -28,8 +28,9 @@
 (use-package auto-package-update
   :config
   (setq auto-package-update-delete-old-versions t)
-  (setq auto-package-update-hide-results t)
-  (auto-package-update-maybe))
+  (setq auto-package-update-hide-results t))
+;; (auto-package-update-maybe)
+;; (auto-package-update-now)
 
 (require 'bind-key)
 
