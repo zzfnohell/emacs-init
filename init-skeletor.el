ac4d0860804268e304dd8bead8278af681939c4a
John <zzfnohell@gmail.com>
2021-03-20 19:39:01 +0800

    skeletor

--- init-skeletor.el
+++ init-skeletor.el
@@ -8,6 +8,26 @@
   :custom
   (skeletor-user-directory "~/.emacs.d/init/init-skeletor-projects")
 	:config
+	(skeletor-define-template "conan-app"
+		:title "Conan App"
+		:default-license (rx bol "gpl")
+		:substitutions
+		'(("__TITLE__" . (lambda () (read-string "Description: ")))))
+	(skeletor-define-template "conan-lib"
+		:title "Conan App"
+		:default-license (rx bol "gpl")
+		:substitutions
+		'(("__TITLE__" . (lambda () (read-string "Description: ")))))
+	(skeletor-define-template "cmake-project"
+		:title "CMake Project"
+		:default-license (rx bol "gpl")
+		:substitutions
+		'(("__TITLE__" . (lambda () (read-string "Description: ")))))
+	(skeletor-define-template "cmake-project-swig"
+		:title "CMake Project (SWIG)"
+		:default-license (rx bol "gpl")
+		:substitutions
+		'(("__TITLE__" . (lambda () (read-string "Description: ")))))
 	(skeletor-define-template "skewer-index-html-package"
 		:title "Skewer Index Html"
 		:no-license? t
