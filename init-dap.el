2fc745f9f1e7c361634dfc59d094214878c2a9dc
Zongfeng Zhang <zongfeng.zhang@nufront.com>
2021-04-02 16:17:34 +0800

    dap mode

--- init-dap.el
+++ init-dap.el
@@ -5,11 +5,6 @@
 
 ;;; Code:
 
-
-;; Use the Debug Adapter Protocol for running tests and debugging
-;; Posframe is a pop-up tool that must be manually installed for dap-mode
-(use-package posframe)
-
 (use-package dap-mode
   :after (:all lsp-mode lsp-java)
   :config
@@ -20,6 +15,7 @@
   ;; use tooltips for mouse hover
   ;; if it is not enabled `dap-mode' will use the minibuffer.
   (tooltip-mode 1)
+	(dap-ui-controls-mode 1)
   
   (require 'dap-gdb-lldb)
   (dap-gdb-lldb-setup)
