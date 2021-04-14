18de9b94deec5ba65da90028984419660f3c2b32
John <zzfnohell@users.noreply.github.com>
2021-04-14 22:00:54 +0800

    flycheck

--- init-flycheck.el
+++ init-flycheck.el
@@ -6,18 +6,33 @@
 ;;; Code:
 
 (use-package flycheck
-  :init (global-flycheck-mode))
+  :init
+	(global-flycheck-mode)
+	:config
+	(message "init-flycheck/config flycheck."))
+
+(use-package flycheck-flow
+	:ensure t
+  :after (:all flycheck)
+  :config
+	(message "init-flycheck/config flycheck flow.")
+	(flycheck-add-mode 'javascript-flow 'flow-mode)
+	(flycheck-add-mode 'javascript-eslint 'flow-mode)
+	(flycheck-add-next-checker 'javascript-flow 'javascript-eslint))
+
+
 
 (use-package flycheck-clang-analyzer
   :config
+	(message "init-flycheck/config flycheck clang analyzer.")
   (flycheck-clang-analyzer-setup))
 
 (use-package flycheck-plantuml
   :after (:all flycheck plantuml-mode)
   :config
-  (with-eval-after-load 'flycheck
-    (require 'flycheck-plantuml)
-    (flycheck-plantuml-setup)))
+	(message "init-flycheck/config flycheck plantuml.")
+	(require 'flycheck-plantuml)
+	(flycheck-plantuml-setup))
 
 (provide 'init-flycheck)
 
