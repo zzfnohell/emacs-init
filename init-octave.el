61b569e57bac83e5902d4f2115740caeaa857f38
Zongfeng Zhang <396423619@qq.com>
2021-08-26 16:28:50 +0800

    cleanup octave

--- init-octave.el
+++ init-octave.el
@@ -1,26 +1,7 @@
-;;; init-octave-mod.el --- Octave
+;;; init-octave.el --- Octave
 
 
 ;;; Code:
-(autoload 'octave-mode "octave-mod" nil t)
-(setq auto-mode-alist (cons '("\\.m$" . octave-mode) auto-mode-alist))
-
-(add-hook 'octave-mode-hook
-          (lambda () (abbrev-mode 1)
-            (auto-fill-mode 1)
-            (if (eq window-system 'x)
-                (font-lock-mode 1))))
-
-
-;; (use-package ac-octave
-;;   
-;;   :config
-;;   (progn
-;;     (require 'ac-octave)
-;;     (defun ac-octave-mode-setup ()
-;;       (setq ac-sources '(ac-complete-octave)))
-;;     (add-hook 'octave-mode-hook
-;;           '(lambda () (ac-octave-mode-setup)))))
 
 (provide 'init-octave)
 
