da3e84e7c9cf0d6868ccde4c9eb0330de6cb68b8
Zongfeng Zhang <396423619@qq.com>
2021-08-26 17:07:15 +0800

    note org

--- misc/templates/cedet-projects-template.el
+++
@@ -1,8 +0,0 @@
-(ede-cpp-root-project
- "template-project-name"
- :file "/path/to/project/Makefile"
- :include-path '("include/" "include1/")
- :system-include-path
- '("/usr/include"
-	 "/usr/local/include"
-	 "/path/to/sys/include"))
