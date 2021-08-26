1221ba0bd15400e924f62735f4eba29d504df6b5
Zongfeng Zhang <396423619@qq.com>
2021-08-26 16:48:17 +0800

    note

--- cedet-win-projects.el
+++
@@ -1,9 +0,0 @@
-(ede-cpp-root-project
- :name "win-cpp-root"
- :file "~/.emacs.d/cedet-projects/Makefile"
- :system-include-path
- '("c:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0/shared/"
-   "c:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0/um/"
-   "c:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0/ucrt/"
-   "c:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0/winrt/"
-   "c:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0/cppwinrt/winrt/"))
