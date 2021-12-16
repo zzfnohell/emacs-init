407d77288ac1878d43b437436dfc7d66573f5e98
John <zzfnohell@users.noreply.github.com>
2021-12-16 23:30:20 +0800

    vertico offical site

--- init-minibuffer.el
+++ init-minibuffer.el
@@ -2,19 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-
-(use-package orderless
-  :ensure t
-  :custom (completion-styles '(orderless)))
-
-(use-package vertico
-  :after (:all orderless embark)
-  :config
-  (add-hook 'after-init-hook 'vertico-mode)
-  (require 'orderless)
-  (define-key vertico-map (kbd "C-c C-o") 'embark-export)
-  (define-key vertico-map (kbd "C-c C-c") 'embark-act))
-
 (use-package affe
   :config
   ;; Manual preview key for `affe-grep'
@@ -170,6 +157,45 @@
   ;; auto-updating embark collect buffer
   :hook
   (embark-collect-mode . consult-preview-at-point-mode))
+
+
+;; Enable vertico
+(use-package vertico
+  :init
+  (vertico-mode)
+
+  ;; Different scroll margin
+  ;; (setq vertico-scroll-margin 0)
+
+  ;; Show more candidates
+  ;; (setq vertico-count 20)
+
+  ;; Grow and shrink the Vertico minibuffer
+  ;; (setq vertico-resize t)
+
+  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+  ;; (setq vertico-cycle t)
+  )
+
+;; Optionally use the `orderless' completion style. See
+;; `+orderless-dispatch' in the Consult wiki for an advanced Orderless style
+;; dispatcher. Additionally enable `partial-completion' for file path
+;; expansion. `partial-completion' is important for wildcard support.
+;; Multiple files can be opened at once with `find-file' if you enter a
+;; wildcard. You may also give the `initials' completion style a try.
+(use-package orderless
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
 
 
 (provide 'init-minibuffer)
