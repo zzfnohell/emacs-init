1c2399b1fe46cee5f21a92f602aaf1ca1b91ea34
Zongfeng Zhang <zzfnohell@outlook.com>
2021-12-21 17:03:00 +0900

    enable theme

--- init-theme.el
+++ init-theme.el
@@ -5,6 +5,11 @@
 
 ;;; Code:
 
+(use-package color-theme-sanityinc-solarized
+	:ensure t)
+(use-package color-theme-sanityinc-tomorrow
+	:ensure t)
+
 (provide 'init-theme)
 
 ;;; init-theme.el ends here
