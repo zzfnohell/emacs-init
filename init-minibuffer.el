6a5ce0822193f9b2c9c8ba7420bd391579201fc8
Zongfeng Zhang <zzfnohell@gmail.com>
2022-07-09 22:24:41 +0800

    embark consult

--- init-minibuffer.el
+++ init-minibuffer.el
@@ -151,6 +151,16 @@
                  (window-parameters (mode-line-format . none)))))
 
 
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :ensure t
+  :after (embark consult)
+  :demand t ; only necessary if you have the hook below
+  ;; if you want to have consult previews as you move around an
+  ;; auto-updating embark collect buffer
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
 ;; Enable vertico
 (use-package vertico
   :init
