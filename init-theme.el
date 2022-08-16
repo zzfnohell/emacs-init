52112d5f40242c649b6bc687c40c34cbb63691a8
Zongfeng Zhang <zzfnohell@outlook.com>
2022-08-16 22:44:14 +0900

    default themes

--- init-theme.el
+++ init-theme.el
@@ -4,19 +4,6 @@
 ;; 
 
 ;;; Code:
-
-;; (use-package doneburn-theme
-;;   :ensure t
-;;   :config
-;;   (load-theme 'doneburn 'no-confirm))
-
-
-(use-package color-theme-sanityinc-tomorrow
-  :ensure t
-  :config
-  (require 'color-theme-sanityinc-tomorrow)
-  (color-theme-sanityinc-tomorrow-day))
-
 (provide 'init-theme)
 
 ;;; init-theme.el ends here
