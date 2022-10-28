afb5414508d72fc96fe4b9f25747a5924ad2abbc
Zongfeng Zhang <396423619@qq.com>
2022-10-28 19:17:51 +0800

    dap mode

--- init-dap.el
+++ init-dap.el
@@ -8,16 +8,17 @@
 (use-package dap-mode
   :ensure t
   :defer t
+  :after (lsp-mode lsp-java)
   :config
-	(dap-auto-configure-mode)
+  (setq dap-auto-configure-features '(sessions locals controls tooltip))
 
   (require 'dap-gdb-lldb)
   (dap-gdb-lldb-setup)
   (require 'dap-go)
   (require 'dap-chrome)
   (require 'dap-python)
-  (require 'dap-java)
   (require 'dap-pwsh)
+  (require 'dap-netcore)
   (require 'dap-node))
 
 (use-package realgud
