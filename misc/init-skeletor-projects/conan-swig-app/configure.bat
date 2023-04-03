ccc0de38c3de04ffbda24b50bba2489dccfa225a
Zongfeng Zhang <396423619@qq.com>
2023-04-03 13:58:24 +0800

    include dirs

--- misc/init-skeletor-projects/conan-swig-app/configure.bat
+++ misc/init-skeletor-projects/conan-swig-app/configure.bat
@@ -1,8 +1,5 @@
-PUSHD build 
-conan install  --build missing ../
-REM conan install -s build_type=Debug ../../
-cmake -S ../  -G "Visual Studio 17 2022" -A x64  -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=install
 
-POPD
+conan install . --output-folder build --build=missing
 
-REM cmake -S . -B build/release -G "Visual Studio 16 2019" -A x64 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=install
+cd build
+cmake -S ..  -G "Visual Studio 17 2022" -A x64  -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE="conan_toolchain.cmake"
