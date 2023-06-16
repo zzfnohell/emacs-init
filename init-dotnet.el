2e404543d7cf1191d0fa70cd439b50171d24a821
Zongfeng Zhang <396423619@qq.com>
2023-06-16 22:46:57 +0800

    remove csharp mode

--- init-dotnet.el
+++ init-dotnet.el
@@ -8,34 +8,6 @@
 (use-package csproj-mode
   :ensure t)
 
-(defun init-csharp/csharp-mode-setup ()
-  (omnisharp-mode)
-  (company-mode)
-  (flycheck-mode)
-
-  (yas-minor-mode-on)
-  (turn-on-auto-revert-mode)
-
-  (setq c-syntactic-indentation t)
-  (c-set-style "ellemtel")
-  (setq c-basic-offset 4)
-  (setq truncate-lines t)
-  (setq tab-width 4)
-
-  (electric-pair-local-mode 1) ;; Emacs 25
-
-  (local-set-key (kbd "C-c r r") 'omnisharp-run-code-action-refactoring)
-  (local-set-key (kbd "C-c C-c") 'recompile))
-
-;; CALL omnisharp-install-server
-;; WIN scoop install omnisharp
-(use-package omnisharp
-  :ensure t
-  :init
-  (setq omnisharp-server-executable-path "OmniSharp.exe")
-  :config
-  (add-hook 'csharp-mode-hook 'init-csharp/csharp-mode-setup t))
-
 (use-package fsharp-mode
   :ensure t
   :mode
