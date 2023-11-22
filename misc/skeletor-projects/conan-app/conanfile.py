2a36cecc17893aa1bde5cca37ad9b3269ba95bc7
Zongfeng Zhang <396423619@qq.com>
2023-11-22 15:14:37 +0800

    conafile py

---
+++ misc/skeletor-projects/conan-app/conanfile.py
@@ -0,0 +1,35 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout, CMakeToolchain, CMake
+
+
+class ConanApplication(ConanFile):
+    package_type = "application"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps"
+    options = {"shared": [True, False], "build_tests": [True, False]}
+    default_options = {
+        "sqlite3/*:shared": True,
+        "lua/*:shared": True,
+        "shared": False,
+        "build_tests": False
+    }
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = False
+        tc.generate()
+
+    def requirements(self):
+        requirements = self.conan_data.get('requirements', [])
+        for requirement in requirements:
+            self.requires(requirement)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        # here you can run CTest, launch your binaries, etc
+        cmake.test()
