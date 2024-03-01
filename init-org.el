4af3e34fca5cee7c6382f8fa41a6c6e4903e7ae6
John <396423619@qq.com>
2024-03-01 10:58:02 +0800

    delete org roam

--- init-org.el
+++ init-org.el
@@ -166,36 +166,6 @@
   (org-babel-do-load-languages 'org-babel-load-languages org-babel-load-languages)
   (add-to-list 'org-babel-tangle-lang-exts '("cypher" . "cypher")))
 
-(use-package org-roam
-	:ensure t
-;;	:custom
-;;	(org-roam-directory (file-truename "~/org/roam/"))
-	:bind (("C-c n l" . org-roam-buffer-toggle)
-				 ("C-c n f" . org-roam-node-find)
-				 ("C-c n g" . org-roam-graph)
-				 ("C-c n i" . org-roam-node-insert)
-				 ("C-c n c" . org-roam-capture)
-				 ;; Dailies
-				 ("C-c n j" . org-roam-dailies-capture-today))
-	:config
-	(org-roam-db-autosync-mode)
-	;; If using org-roam-protocol
-	(require 'org-roam-protocol))
-
-(use-package websocket
-  :after org-roam
-  :ensure t)
-
-
-(use-package org-roam-ui
-  :ensure t
-  :after org-roam
-  :hook (after-init . org-roam-ui-mode)
-  :config
-  (setq org-roam-ui-sync-theme t
-        org-roam-ui-follow t
-        org-roam-ui-update-on-save t
-        org-roam-ui-open-on-start t))
 
 (use-package deft
   :ensure t
--- init-vc.el
+++ init-vc.el
@@ -18,12 +18,30 @@
 (use-package magit
   :ensure t
   :commands magit
-  :if (not  (eq system-type 'windows-nt))
+  :custom
+  (magit-auto-revert-mode nil)
+  (magit-diff-arguments (quote ("--no-ext-diff" "-M" "-C")))
+  (magit-diff-refine-hunk t)
+  (magit-expand-staged-on-commit (quote full))
+  (magit-fetch-arguments (quote ("--prune")))
+  (magit-log-auto-more t)
+  (magit-log-cutoff-length 20)
+  (magit-no-confirm (quote (stage-all-changes unstage-all-changes)))
+  (magit-process-connection-type nil)
+  (magit-push-always-verify nil)
+  (magit-push-arguments (quote ("--set-upstream")))
+  (magit-refresh-file-buffer-hook nil)
+  (magit-save-some-buffers nil)
+  (magit-set-upstream-on-push (quote askifnotset))
+  (magit-stage-all-confirm nil)
+  (magit-status-verbose-untracked nil)
+  (magit-unstage-all-confirm nil)
+  (magithub-message-confirm-cancellation nil)
+  (magithub-use-ssl t)
   :config
+  (delete 'Git vc-handled-backends)
   (when (eq system-type 'windows-nt)
-    (setq magit-refresh-status-buffer nil)
-    (setq auto-revert-buffer-list-filter
-          'magit-auto-revert-repository-buffer-p)))
+    (setq magit-refresh-status-buffer nil)))
 
 (use-package vc-fossil
 	:ensure t
