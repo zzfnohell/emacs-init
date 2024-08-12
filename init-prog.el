e00cdbe1c7052ac7123563245520ea2525649996
John Zhang <396423619@qq.com>
2024-08-12 21:38:17 +0800

    treesit auto

--- init-prog.el
+++ init-prog.el
@@ -16,6 +16,15 @@
     :ensure t
     :defer t)
 
+  (use-package treesit-auto
+    :ensure t
+    :defer t
+    :custom
+    (treesit-auto-install 'prompt)
+    :config
+    (treesit-auto-add-to-auto-mode-alist 'all)
+    (global-treesit-auto-mode))
+
   (require 'treesit)
 
   (setq major-mode-remap-alist
