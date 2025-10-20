3d59212fe239ca134e8dda63e47b547cd7fb9ee6
zhangzongfeng <zongfeng.zhang@nufront.com>
2025-10-20 17:28:29 +0800

    refine daemonfont config

--- init-font.el
+++ init-font.el
@@ -6,75 +6,79 @@
 
 ;;; Code:
 
-(defvar emacs-english-font nil
+(defvar emacs-english-font "Fira Code"
   "The font name of English.")
 
-(defvar emacs-cjk-font nil
+(defvar emacs-cjk-font "等距更纱黑体 SC"
   "The font name for CJK.")
 
-(defvar emacs-font-size-pair nil
-  "Default font size pair for (english . chinese)")
+(defvar emacs-font-size-pair '(13 . 16)
+  "Default font size pair for (english . chinese).")
 
-(defvar emacs-font-size-pair-list nil
-  "This list is used to store matching (englis . chinese) font-size.")
+(defvar emacs-font-size-pair-list
+  '((5 . 6)
+    (10 . 12) 
+    (13 . 16) 
+    (15 . 18) 
+    (17 . 20)
+    (19 . 22)
+	(20 . 24) 
+	(21 . 26) 
+	(24 . 28)
+    (26 . 32) 
+	(28 . 34) 
+	(30 . 36) 
+	(34 . 40) 
+	(36 . 44))
+  "Font size pairs for scaling (english . chinese).")
 
 (defun font-exist-p (fontname)
-  "test if this font is exist or not."
-  (if (or (not fontname) (string= fontname ""))
-      nil
-    (if (not (x-list-fonts fontname))
-        nil t)))
+  "Return t if FONTNAME exists."
+  (and fontname (x-list-fonts fontname)))
 
 (defun set-font (english chinese size-pair)
-  "Setup emacs English and Chinese font on x window-system."
-  (if (font-exist-p english)
-      (set-frame-font (format "%s:pixelsize=%d" english (car size-pair)) t))
-
-  (if (font-exist-p chinese)
-      (dolist (charset '(kana han symbol cjk-misc bopomofo))
-        (set-fontset-font (frame-parameter nil 'font) charset
-                          (font-spec :family chinese :size (cdr size-pair))))))
+  "Setup English and Chinese fonts for the current frame."
+  (when (font-exist-p english)
+    (set-frame-font (format "%s:pixelsize=%d" english (car size-pair)) t))
+  (when (font-exist-p chinese)
+    (dolist (charset '(kana han symbol cjk-misc bopomofo))
+      (set-fontset-font t charset
+                        (font-spec :family chinese :size (cdr size-pair))))))
 
 (defun emacs-step-font-size (step)
-  "Increase/Decrease emacs's font size."
-  (let ((scale-steps emacs-font-size-pair-list))
-    (if (< step 0) (setq scale-steps (reverse scale-steps)))
+  "Increase/Decrease Emacs font size according to list."
+  (let ((scale-steps (if (< step 0)
+                         (reverse emacs-font-size-pair-list)
+                       emacs-font-size-pair-list)))
     (setq emacs-font-size-pair
           (or (cadr (member emacs-font-size-pair scale-steps))
               emacs-font-size-pair))
     (when emacs-font-size-pair
-      (message "emacs font size set to %.1f" (car emacs-font-size-pair))
+      (message "Emacs font size set to %.1f" (car emacs-font-size-pair))
       (set-font emacs-english-font emacs-cjk-font emacs-font-size-pair))))
 
 (defun increase-emacs-font-size ()
-  "Decrease emacs's font-size acording emacs-font-size-pair-list."
-  (interactive) (emacs-step-font-size 1))
+  "Increase font size."
+  (interactive)
+  (emacs-step-font-size 1))
 
 (defun decrease-emacs-font-size ()
-  "Increase emacs's font-size acording emacs-font-size-pair-list."
-  (interactive) (emacs-step-font-size -1))
+  "Decrease font size."
+  (interactive)
+  (emacs-step-font-size -1))
 
-(setq list-faces-sample-text
-      (concat
-       "ABCDEFTHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz\n"
-       "11223344556677889900       一二三四五六七八九十"))
+(defun init-font/setup-font (&optional frame)
+  "Apply font setup for FRAME (or current frame)."
+  (with-selected-frame (or frame (selected-frame))
+    (set-font emacs-english-font emacs-cjk-font emacs-font-size-pair)))
 
-(when window-system
-  ;; setup change size font, base on emacs-font-size-pair-list
-  (global-set-key (kbd "C-M-=") 'increase-emacs-font-size)
-  (global-set-key (kbd "C-M--") 'decrease-emacs-font-size)
-
-  ;; setup default english font and cjk font
-  (setq emacs-english-font "Fira Code")
-  (setq emacs-cjk-font "等距更纱黑体 SC")
-  (setq emacs-font-size-pair '(13 . 16))
-  (setq emacs-font-size-pair-list '(( 5 .  6) (10 . 12)
-                                    (13 . 16) (15 . 18) (17 . 20)
-                                    (19 . 22) (20 . 24) (21 . 26)
-                                    (24 . 28) (26 . 32) (28 . 34)
-                                    (30 . 36) (34 . 40) (36 . 44)))
-  ;; Setup font size based on emacs-font-size-pair
-  (set-font emacs-english-font emacs-cjk-font emacs-font-size-pair))
+;; === Font setup hooks ===
+(if (daemonp)
+    ;; Daemon 模式：等待 frame 创建
+    (add-hook 'after-make-frame-functions #'init-font/setup-font)
+  ;; 普通 GUI 启动
+  (when (display-graphic-p)
+    (init-font/setup-font)))
 
 (when (and (>= emacs-major-version 28) window-system)
   (use-package ligature
