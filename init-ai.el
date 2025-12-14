bbc0d7e71ef8237708fa821b6afdbb13b7a808ec
John <zzfnohell@gmail.com>
2025-12-14 14:02:53 +0800

    ai

--- init-ai.el
+++ init-ai.el
@@ -9,8 +9,6 @@
   :config
   (gptel-agent-update)
   (setq
-   gptel-use-curl "D:/msys64/mingw64/bin/curl.exe"
-   gptel-proxy "http://sshost:2001"
    gptel-model 'gemini-pro-latest
    gptel-backend (gptel-make-gemini "Gemini"
                    :key "AIzaSyAbPUzjP1erOZF3sc_fIlEpTl-Y312SpWw"
@@ -18,11 +16,12 @@
 
 (use-package agent-shell
   :ensure t
-  :ensure-system-package
-  ;; Add agent installation configs here
-  ((claude . "brew install claude-code")
-   (qwen . "npm install -g @qwen-code/qwen-code@latest")
-   (claude-code-acp . "npm install -g @zed-industries/claude-code-acp")))
+  ;; :ensure-system-package
+  ;; ;; Add agent installation configs here
+  ;; ((claude . "brew install claude-code")
+  ;;  (qwen . "npm install -g @qwen-code/qwen-code@latest")
+  ;;  (claude-code-acp . "npm install -g @zed-industries/claude-code-acp"))
+  )
 
 
 (provide 'init-ai)
